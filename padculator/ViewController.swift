6228d6d138e4db7e952809e8d42d47d93d954d27
Seokjin Yoon <sjy@hansung.ac.kr>
2021-05-16 10:58:02 +0900

    First commit

---
+++ padculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  padculator
+//
+//  Created by mac024 on 2021/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
