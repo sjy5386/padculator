602f4c3e7ec122e714d591293738c062b2bae19a
Seokjin Yoon <sjy@hansung.ac.kr>
2021-05-16 16:35:07 +0900

    Update ViewController

--- padculator/ViewController.swift
+++ padculator/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var equationTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func resultButton(_ sender: UIButton) {
+        let equation = equationTextField.text
+        let result = calculate(equation: equation!)
+        resultLabel.text = String(result!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
