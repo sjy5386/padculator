0c35dc83a1eeb38ca098d985e7371831bb61f159
Seokjin Yoon <sjy@hansung.ac.kr>
2021-05-28 16:03:17 +0900

    Add History

---
+++ padculator/History.swift
@@ -0,0 +1,18 @@
+//
+//  History.swift
+//  padculator
+//
+//  Created by mac024 on 2021/05/28.
+//
+
+import Foundation
+
+class History {
+    var equation: String
+    var result: Double
+    
+    init(equation: String, result: Double) {
+        self.equation = equation
+        self.result = result
+    }
+}
